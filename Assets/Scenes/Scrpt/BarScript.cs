4c2e3a3c8383620fc86ee4ea7d441a935ba952f3
watabe471 <watal.0302@gmail.com>
2025-05-14 15:18:16 +0900

    バーの移動を編集しました

--- Assets/Scenes/Scrpt/BarScript.cs
+++ Assets/Scenes/Scrpt/BarScript.cs
@@ -4,52 +4,14 @@ using UnityEngine;
 
 public class BarScript : MonoBehaviour
 {
-    // 移動時の加算用変数
-    float vx = 0;
-    float vy = 0;
+    public float moveSpeed = 20.0f; // 移動速度
 
-    // 移動スピード
-    public float speed = 5;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //1左右の入力を取得
-        //float myX = Input.GetAxis("Horizontal");
-        //位置情報の変更
-        //transform.position += new Vector3(myX * 10, 0, 0) * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // 水平方向の移動
+        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;   // 垂直方向の移動
 
-        // 毎フレーム数値を初期化
-        vx = 0;
-        vy = 0;
-
-        // 横移動
-        if (Input.GetKey(KeyCode.A))
-        {
-            vx = -speed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            vx = speed;
-        }
-
-        // 縦移動
-        if (Input.GetKey(KeyCode.W))
-        {
-            vy = speed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            vy = -speed;
-        }
-
-        // 実際の移動処理
-        this.transform.Translate(vx / 50, vy / 50, 0);
+        transform.position += new Vector3(moveX, moveY, 0); // オブジェクトの位置を更新
     }
 }
+
