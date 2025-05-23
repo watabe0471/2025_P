86c625ed2c23868566c27ba8639f2987d881a710
yuga_yamada0614 <nhs20301@nhs.hal.ac.jp>
2025-05-23 16:52:00 +0900

    背景の追加

--- Assets/Scenes/yamada's_file/ChangeScean.cs
+++ Assets/Scenes/yamada's_file/ChangeScean.cs
@@ -8,34 +8,52 @@ public class ChangeScean : MonoBehaviour
 
     // ステージ名
     [Header("ステージ")]
-    [SerializeField] Scene Stage1;
+    //[SerializeField] Scene Stage1;
+    [SerializeField] string Stage1;
+    private GameObject[] BallTag;
 
     public enum e_Stage     // ステージの識別
-        {
-            Stage1,
-            Stage2,
-            Stage3,
-            Stage4
-        }
+    {
+        Stage1,
+        Stage2,
+        Stage3,
+        Stage4
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        BallTag = GameObject.FindGameObjectsWithTag("Ball");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < BallTag.Length; i++)
+        {
+            if (BallTag[i] != null)
+            {
+                break;
+            }
+            SceneChange();
+        }
     }
 
-    public void SceneChange(e_Stage NowStage)
+
+
+
+
+
+
+    public void SceneChange()
     {
+        // テスト
+        e_Stage NowStage = e_Stage.Stage1;
+
 
         switch(NowStage)
         {
             case e_Stage.Stage1:
-                SceneManager.LoadScene(Stage1.name);
+                SceneManager.LoadScene(Stage1);
                 break;
             case e_Stage.Stage2:
 
