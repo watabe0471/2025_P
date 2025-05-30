b0f65b42edc2fcd1c45f0f773ee9741c42a5353f
watabe471 <watal.0302@gmail.com>
2025-05-30 16:22:21 +0900

    ボールが非破壊ブロックに当たったらマテリアルが変わるようにしました

---
+++ Assets/Scenes/Scrpt/NonBrakeScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonBrakeScript : MonoBehaviour
+{
+    public Material changedMaterial;
+
+    private Dictionary<GameObject, Material> originalMaterial = new Dictionary<GameObject, Material>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // ボールがぶつかってきた場合のみ処理
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            string blockTypeTag = this.gameObject.tag;
+
+            // 同じ種類のブロックをすべて取得
+            GameObject[] sameTypeBlocks = GameObject.FindGameObjectsWithTag(blockTypeTag);
+
+            foreach (GameObject block in sameTypeBlocks)
+            {
+                Renderer rend = block.GetComponent<Renderer>();
+                if (rend != null)
+                {
+                    if(!originalMaterial.ContainsKey(block))
+                    {
+                        originalMaterial[block] = rend.material;
+                    }
+
+                    rend.material = changedMaterial;
+                }
+            }
+
+            StartCoroutine(RestoreMaterialAfterDelay(sameTypeBlocks, 5f));
+        }
+    }
+
+    private IEnumerator RestoreMaterialAfterDelay(GameObject[] blocks,float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach(GameObject block in blocks)
+        {
+            Renderer rend = block.GetComponent<Renderer>();
+            if(rend != null && originalMaterial.ContainsKey(block))
+            {
+                rend.material = originalMaterial[block];
+            }
+        }
+
+        foreach(GameObject block in blocks)
+        {
+            if (originalMaterial.ContainsKey(block))
+                originalMaterial.Remove(block);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
