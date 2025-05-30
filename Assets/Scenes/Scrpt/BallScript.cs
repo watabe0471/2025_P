83412cf92eb917ee8d89302828ec6156a680e184
yuga_yamada0614 <nhs20301@nhs.hal.ac.jp>
2025-05-30 14:57:10 +0900

    球が画面外に出たらゲームオーバーに

--- Assets/Scenes/Scrpt/BallScript.cs
+++ Assets/Scenes/Scrpt/BallScript.cs
@@ -7,30 +7,39 @@ public class BallScript : MonoBehaviour
     public float speed = 2000f;
 
     private Rigidbody rb;
+    private bool GameState = false; // ゲームスタートフラグ
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+       
     }
 
     // 毎フレーム速度をチェックする
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && GameState == false)
         {
             LaunchBall();
+            GameState = true; // ゲームがスタートした
+
         }
     }
 
     void LaunchBall()
     {
         Vector3 randomDirection = new Vector3(
-            Random.Range(-0.3f, 0.3f),
+            Random.Range(0f, 3f),
             1f,
             0f
         ).normalized;
 
         rb.AddForce(randomDirection * speed, ForceMode.Impulse);
+    }
+    // カメラ外に出たら作動する関数
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);    
     }
 }
