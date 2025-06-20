b523e36aa290ada57a93df565db6fb019f975823
yuga_yamada0614 <nhs20301@nhs.hal.ac.jp>
2025-06-20 17:09:47 +0900

    no message

--- Assets/Scenes/Scrpt/BarScript.cs
+++ Assets/Scenes/Scrpt/BarScript.cs
@@ -6,7 +6,8 @@ public class BarScript : MonoBehaviour
 {
     public float moveSpeed = 20.0f; // 移動速度
     public float speed = 10.0f; // 球の移動速度
-
+    private bool StartGame = true;
+    GameObject newBall = null;
 
     [Header("球のオブジェクト")]
     [SerializeField] private GameObject ballPrefab;
@@ -15,7 +16,7 @@ public class BarScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        // LaunchBall(ballPrefab);   // 初期球を設置
 
     }
 
@@ -28,18 +29,30 @@ public class BarScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.B))
         {
-            LaunchBall();
+            LaunchBall(ballPrefab);
         }
 
+        Vector3 ballShootPos = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
+        if (StartGame == true)
+        {
+            newBall = Instantiate(ballPrefab, ballShootPos, Quaternion.identity);
+            newBall.tag = "Ball";   // クローンにタグBallを追加
+            // newBall.transform.parent = transform;
+
+            StartGame = false;
+        }
+        
+         newBall.transform.position = transform.position + new Vector3(0.0f, 1.0f, 0.0f);
 
         }
-    public void LaunchBall()
+    public void LaunchBall(GameObject newBall)
     {
+        
         // 球を生成
         Vector3 ballShootPos = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
-        GameObject newBall = Instantiate(ballPrefab,ballShootPos, Quaternion.identity);
+        newBall = Instantiate(ballPrefab,ballShootPos, Quaternion.identity);
         newBall.tag = "Ball";   // クローンにタグBallを追加
-        
+    
         Vector3 randomDirection = new Vector3(
     Random.Range(0f, 3f),
     1f,
